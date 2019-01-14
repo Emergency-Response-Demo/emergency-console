faabaa718d98b7346938571de267902009fef42e
Mike Echevarria <mechevar@redhat.com>
2019-01-14 15:06:37 -0500

    fixes for mobile viewing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, HostListener } from '@angular/core';
 import { faEraser, faSignOutAlt, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { KeycloakService } from '../keycloak.service';
 import { AlertModel } from '../alerts/alert-model';
@@ -18,14 +18,6 @@ export class HeaderComponent implements OnInit {
   alerts: AlertModel[];
   logoutUrl: string;
 
-  constructor(private alertService: AlertService, private renderer: Renderer2, private keycloakService: KeycloakService) {
-    this.eraseIcon = faEraser;
-    this.logoutIcon = faSignOutAlt;
-    this.sidebarVisible = true;
-    this.alerts = new Array();
-    this.username = '';
-  }
-
   toggleSidebar() {
     this.sidebarVisible = !this.sidebarVisible;
     if (this.sidebarVisible === false) {
@@ -33,6 +25,24 @@ export class HeaderComponent implements OnInit {
     } else {
       this.renderer.addClass(document.body, 'sidebar-show');
     }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  checkForMobile(event?) {
+    if (window.innerWidth < 425) {
+      this.toggleSidebar();
+    }
+  }
+
+  constructor(private alertService: AlertService, private renderer: Renderer2, private keycloakService: KeycloakService) {
+    this.eraseIcon = faEraser;
+    this.logoutIcon = faSignOutAlt;
+    this.sidebarVisible = true;
+    this.alerts = new Array();
+    this.username = '';
+
+    // hide sidebar by default on mobile
+    this.checkForMobile();
   }
 
   ngOnInit(): void {
