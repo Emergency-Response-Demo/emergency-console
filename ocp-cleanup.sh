bfd8eec6821b80d8a29abca8922e427de8afbeea
Mike Echevarria <mechevar@redhat.com>
2019-03-14 10:28:46 -0400

    refactor to emergency-console

--- ocp-cleanup.sh
+++ ocp-cleanup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 oc project naps-emergency-response
-oc delete all --selector app=frontend-manager
+oc delete all --selector app=emergency-console
--- ocp-deploy.sh
+++ ocp-deploy.sh
@@ -4,7 +4,7 @@ oc project naps-emergency-response
 
 oc new-app \
 --image-stream=nodejs \
---code=https://github.com/NAPS-emergency-response-project/frontend-manager \
---name=frontend-manager
+--code=https://github.com/NAPS-emergency-response-project/emergency-console \
+--name=emergency-console
 
-oc expose svc/frontend-manager
+oc expose svc/emergency-console
