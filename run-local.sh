53c794933c488aa4c2451f6bb279b96108899337
Mike Echevarria <mechevar@redhat.com>
2019-03-19 15:44:53 -0400

    add icons for shelter and current location

--- run-local.sh
+++ run-local.sh
@@ -5,7 +5,7 @@ export INCIDENT="http://incident-service-naps-emergency-response.apps.753d.opens
 export ALERT="http://alert-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
 export RESPONDER="http://responder-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
 export AUTH_URL="https://sso-naps-emergency-response.apps.753d.openshift.opentlc.com/auth"
-export KEYCLOAK="false"
+export KEYCLOAK="true"
 export TOKEN="pk.eyJ1IjoibWVjaGV2YXJyaWEiLCJhIjoiY2pxbXNuMXF0MGwzNTQ5bzJwNGtyMTRqdyJ9.WZfALlPxuOveabQDrroLcQ"
 
 npm run start
