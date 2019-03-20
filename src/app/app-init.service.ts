f5f78dfa436bb72ce8012ee5826b71646b084ccb
Mike Echevarria <mechevar@redhat.com>
2019-03-20 13:06:41 -0400

    make if else statement more readable

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -5,28 +5,30 @@ import { KeycloakService, KeycloakOptions } from 'keycloak-angular';
   providedIn: 'root'
 })
 export class AppInitService {
-  constructor(private keycloak: KeycloakService) {}
+  private options: KeycloakOptions;
+
+  // _env is defined in env.js
+  constructor(private keycloak: KeycloakService) {
+    this.options = {
+      config: {
+        realm: window['_env'].realm,
+        url: window['_env'].url,
+        clientId: window['_env'].clientId
+      },
+      initOptions: {
+        onLoad: 'login-required'
+      }
+    };
+  }
 
   init(): Promise<any> {
     let promise: Promise<any>;
 
-    // _env is defined in env.js
     if (window['_env'].enabled === 'false') {
       console.log('keycloak is not enabled');
       promise = Promise.resolve(true);
     } else {
-      const options: KeycloakOptions = {
-        config: {
-          realm: window['_env'].realm,
-          url: window['_env'].url,
-          clientId: window['_env'].clientId
-        },
-        initOptions: {
-          onLoad: 'login-required'
-        }
-      };
-
-      promise = this.keycloak.init(options);
+      promise = this.keycloak.init(this.options);
     }
 
     return promise;
