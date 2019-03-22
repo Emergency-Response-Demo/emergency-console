d33be94e7c065469ead08ed194d844d0bc47f18c
Mike Echevarria <mechevar@redhat.com>
2019-03-22 11:23:11 -0400

    add logic to go through all the steps and color map appropriately

--- src/app/mission/mission.component.ts
+++ src/app/mission/mission.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message/message.service';
 import { KeycloakService } from 'keycloak-angular';
 import { Responder } from './responder';
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faShip, faPhone, faBriefcaseMedical } from '@fortawesome/free-solid-svg-icons';
-import { MapMouseEvent, LngLatBoundsLike, LngLat, FitBoundsOptions } from 'mapbox-gl';
+import { MapMouseEvent, LngLatBoundsLike, LngLat, FitBoundsOptions, LinePaint, LineLayout, Layer } from 'mapbox-gl';
 import { MissionService } from './mission.service';
 import { LineString } from 'geojson';
 import { AppUtil } from '../app-util';
@@ -17,10 +15,6 @@ import { AppUtil } from '../app-util';
 export class MissionComponent implements OnInit {
   model: Responder = new Responder();
   center: LngLat = new LngLat(-77.886765, 34.210383);
-  userIcon: IconDefinition = faUser;
-  boatIcon: IconDefinition = faShip;
-  phoneIcon: IconDefinition = faPhone;
-  medicalIcon: IconDefinition = faBriefcaseMedical;
   boundsOptions: FitBoundsOptions = {
     padding: 50
   };
@@ -29,11 +23,36 @@ export class MissionComponent implements OnInit {
   start: LngLat;
   end: LngLat;
   bounds: LngLatBoundsLike;
+  missionStatus: string = null;
+
+  readonly GREY = '#a4b7c1';
+  readonly YELLOW = '#ffc107';
+  readonly BLUE = '#20a8d8';
+  readonly RED = '#f86c6b';
+  readonly GREEN = '#4dbd74';
+
+  startStyle: any = {
+    'background-image': 'url(assets/img/location.svg)'
+  };
+  endStyle: any = {
+    'background-image': 'url(assets/img/marker-red.svg)'
+  };
+  lineLayout: LineLayout = {
+    'line-join': 'round',
+    'line-cap': 'round'
+  };
+  linePaint: LinePaint = {
+    'line-color': this.GREY,
+    'line-width': 8
+  };
 
   constructor(private messageService: MessageService, private keycloak: KeycloakService, private missionService: MissionService) {}
 
-  submit(): void {
+  doAvailable(): void {
+    this.missionStatus = 'Available';
+    this.linePaint['line-color'] = this.RED;
     this.messageService.info('You are now available to receive a rescue mission');
+    this.endStyle['background-image'] = 'url(assets/img/marker-red.svg)';
     this.end = new LngLat(-77.94346099447226, 34.21828123440535);
     this.directions = null;
 
@@ -43,8 +62,46 @@ export class MissionComponent implements OnInit {
     });
   }
 
+  doStart(): void {
+    this.messageService.info('Mission started');
+    this.missionStatus = 'Start';
+    this.linePaint['line-color'] = this.YELLOW;
+    this.endStyle['background-image'] = 'url(assets/img/marker-yellow.svg)';
+    this.directions = null;
+
+    this.missionService.getDirections(this.start, this.end).subscribe(res => {
+      this.directions = res.routes[0].geometry;
+      this.bounds = AppUtil.getBounds(this.directions.coordinates);
+    });
+  }
+
+  doPickedUp(): void {
+    this.messageService.info('Victim picked up');
+    this.missionStatus = 'Picked Up';
+    this.linePaint['line-color'] = this.BLUE;
+
+    this.start = this.end;
+    this.end = new LngLat(-77.949, 34.1706);
+    this.endStyle['background-image'] = 'url(assets/img/marker-blue.svg)';
+    this.directions = null;
+
+    this.missionService.getDirections(this.start, this.end).subscribe(res => {
+      this.directions = res.routes[0].geometry;
+      this.bounds = AppUtil.getBounds(this.directions.coordinates);
+    });
+  }
+
+  doRescued(): void {
+    this.messageService.success('Victim rescued');
+    this.missionStatus = null;
+    this.linePaint['line-color'] = this.GREY;
+    this.start = null;
+    this.endStyle['background-image'] = 'url(assets/img/marker-green.svg)';
+    this.directions = null;
+  }
+
   setLocation(event: MapMouseEvent): void {
-    if (event.lngLat) {
+    if (event.lngLat && this.missionStatus === null) {
       this.start = event.lngLat;
     }
   }
