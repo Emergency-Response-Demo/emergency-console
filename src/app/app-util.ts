4d147c6d4a287a16d875b56f31984c446697f480
Mike Echevarria <mechevar@redhat.com>
2019-04-18 10:00:21 -0400

    add method used for initial line drawing object

--- src/app/app-util.ts
+++ src/app/app-util.ts
@@ -16,4 +16,20 @@ export class AppUtil {
     const bounds: LngLatBoundsLike = [lon[0], lat[0], lon.slice(-1)[0], lat.slice(-1)[0]];
     return bounds;
   }
+
+  public static initGeoJson(): GeoJSON.FeatureCollection<GeoJSON.LineString> {
+    return {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'LineString',
+            coordinates: []
+          }
+        }
+      ]
+    };
+  }
 }
