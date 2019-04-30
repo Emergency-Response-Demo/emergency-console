6eb5f5541df7f39a80c88b1311cbf2fcc5ef0d27
jamesfalkner <schtool@gmail.com>
2019-04-30 14:55:53 -0400

    Get link to live process diagram for each Incident from Mission Dashboard

--- run-local.sh
+++ run-local.sh
@@ -5,6 +5,7 @@ export INCIDENT="http://incident-service-naps-emergency-response.apps.753d.opens
 export ALERT="http://alert-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
 export RESPONDER="http://responder-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
 export MISSION="http://mission-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
+export PROCESS_VIEWER="http://process-viewer-naps-emergency-response.apps.753d.openshift.opentlc.com"
 export AUTH_URL="https://sso-naps-emergency-response.apps.753d.openshift.opentlc.com/auth"
 export KEYCLOAK="true"
 export POLLING="10000"
