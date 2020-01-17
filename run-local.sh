b44503400aa22e759558ab2f66835be22d9a6cac
dudash <jason.dudash@gmail.com>
2020-01-17 17:25:12 -0500

    fixing run-local

--- run-local.sh
+++ run-local.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
 export PORT=4200
-export INCIDENT="http://user4-incident-service.apps.cluster-e222.e222.example.opentlc.com"
-export KAFKA_HOST="localhost:9092"
+export INCIDENT="http://incident-service.apps.erdemo-df1a.open.redhat.com"
 export ALERT="http://alert-service-naps-emergency-response.apps.753d.openshift.opentlc.com"
-export RESPONDER="http://user4-responder-service.apps.cluster-e222.e222.example.opentlc.com"
-export MISSION="http://user4-mission-service.apps.cluster-e222.e222.example.opentlc.com"
-export PROCESS_VIEWER="http://user4-process-viewer.apps.cluster-e222.e222.example.opentlc.com"
-export AUTH_URL="https://sso-user-sso.apps.cluster-e222.e222.example.opentlc.com/auth"
-export KEYCLOAK="false"
+export RESPONDER="http://responder-service.apps.erdemo-df1a.open.redhat.com"
+export MISSION="http://mission-service.apps.erdemo-df1a.open.redhat.com"
+export PRIORITY="http://incident-priority-service.apps.erdemo-df1a.open.redhat.com"
+export PROCESS_VIEWER="http://process-viewer.apps.erdemo-df1a.open.redhat.com"
+export AUTH_URL="https://sso-sso.apps.erdemo-df1a.open.redhat.com/auth"
+export KEYCLOAK="true"
 export POLLING="10000"
 export REALM="emergency-realm"
 export CLIENTID="js"
 
-export JASON_MAPBOX_TOKEN=pk.eyJ1IjoiamFzb25yZWRoYXQiLCJhIjoiY2lseHVmZmkwMDVzMTgza3NiazZzZXg4ciJ9.5iDS06m7GN2wuv2hwQePpQ
-export TOKEN=${JASON_MAPBOX_TOKEN}
-
 # https://account.mapbox.com/access-tokens/
-# export TOKEN=${MAPBOX_TOKEN}
+export TOKEN=${MAPBOX_TOKEN}
 
 npm run start
